3029d4e4ce1347850d16aedd126e98d949990874
Student <Student@C0944-00.yosh.ac.il>
2016-03-06 13:59:58 +0200

    First Commit

---
+++ Tr/src/stam.java
@@ -0,0 +1,12 @@
+
+public class stam {
+	
+	public static void name() {
+		System.out.println("Hello");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			name();
+	}
+
+}
